ab0e0220a85eb239ff07045504d6b777753698a1
Alexander Gutierrez <alexander.gutv@gmail.com>
2021-12-26 18:26:51 -0300

    context, response http

--- scriptmanager/context.py
+++ scriptmanager/context.py
@@ -1,18 +1,5 @@
 
-class HttpContext:
-
-    def __init__(self) -> None:
-        self.trigger_id = None
-        self.api_name = None
-        self.namespace = None
-        self.group = None
-        self.method = None
-        self.query_string = {}
-        self.body = {}
-
-
 class Context:
-
     def __init__(self) -> None:
         self.script_id = None
         self.script_name = None
@@ -21,9 +8,6 @@ class Context:
         # http|task|action
         self.trigger_type = None
 
-        # HTTP Context
-        self.http_context = None
-
         # data stored in the system
         # http data context persist on namespace level
         # task data context persist on trigger level
@@ -31,9 +15,18 @@ class Context:
         # script does not store context data
         self.context_store = {}
 
-    def set_value(self, key: str, value):
-        pass
 
-    def get_value(self, key) -> any:
-        pass
+class HttpContext(Context):
+
+    def __init__(self) -> None:
+        super().__init__()
+        # trigger data
+        self.trigger_id = None
+        self.api_name = None
+        self.namespace = None
+        self.group = None
+        self.method = None
+        self.query_string = {}
+        self.body = {}
+
 
--- scriptmanager/script_manager.py
+++ scriptmanager/script_manager.py
@@ -1,13 +1,57 @@
-from scriptmanager.context import Context
+import json
+import sys
+import os
+
+from scriptmanager.context import Context, HttpContext
+
+WORKSPACE = os.getenv("SCRIPT_WORKSPACE")
+RUN_TYPE = os.getenv("RUN_TYPE")
+READ_PIPE = int(os.getenv("READ_PIPE", "0"))
+TRIGGER_CONTEXT = os.getenv("TRIGGER_CONTEXT", "{}")
+
+
+def get_http_context() -> HttpContext:
+    json_context = json.loads(TRIGGER_CONTEXT)
+
+    # try:
+    #     r = os.fdopen(READ_PIPE)
+    #     json_context = json.load(r)
+    # except Exception as e:
+    #     print(e)
+
+    # file_path = f"{WORKSPACE}/context.json"
+    # with open(file_path, 'r', encoding='utf8') as f:
+    #     json_context = json.load(f)
+
+    context = HttpContext()
+    context.script_id = json_context["script_id"]
+    context.script_name = json_context["script_name"]
+    context.run_type = json_context["run_type"]
+    context.trigger_type = json_context["trigger_type"]
+    context.trigger_id = json_context["trigger_id"]
+    context.api_name = json_context["api_name"]
+    context.namespace = json_context["namespace"]
+    context.group = json_context["group"]
+    context.method = json_context["method"]
+    context.query_string = json_context["query_string"]
+    context.body = json_context["body"]
+
+    return context
 
 
 class ScriptManager:
 
     base_url = "http://localhost:8910/__internal"
 
-    def __init__(self, context: Context) -> None:
+    def __init__(self) -> None:
 
-        self.context = context
+        self.context = get_http_context()
+
+    def http_response(self, response: dict) -> None:
+        if type(response) != dict:
+            raise TypeError("Response must be a dictionary")
+
+        sys.stderr.write("__response__=" + json.dumps(response) + "||")
 
     def action(self, act: str, data: dict = None, with_response: bool = False,
                with_callback: bool = False, callback=None, after=None, unit="minute") -> dict:
